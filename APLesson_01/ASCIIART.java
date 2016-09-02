2cf11e5e3d85bd82588cb3d9d074ecdc8db7e51b
Julian Lange <langej0159@gmail.com>
2016-09-02 11:08:13 -0700

    First Lab files

---
+++ APLesson_01/ASCIIART.java
@@ -0,0 +1,12 @@
+public class ASCIIART
+{
+public static void main(String[]args)
+{
+	System.out.println("  ______          __  _                ");
+	System.out.println(" /_  __/  __  _____/ /_(_)___  ____ _    ");
+	System.out.println("   / /   / _ \\/ ___/ __/ / __ \\/ __ `/    ");
+	System.out.println("  / /   /  __ (__  ) /_/ / / / / /_/ / _ _ ");
+	System.out.println("/_/   \\___/____/\\__/_/_/ /_/\\__, (_|_|_)");
+	System.out.println("                          /____/       ");
+}
+}

ce55ed50e8ba9bdc36f585afc8c15c7231fd9a9d
Julian Lange <langej0159@gmail.com>
2016-09-07 12:10:45 -0700

    Lab_02 stuff

---
+++ APLesson_02/Lab_02/Multiplication.java
@@ -0,0 +1,11 @@
+public class Multiplication
+{
+	public static void main(String[]args)
+	{
+		int z = 32;
+		int x = 52;
+		int Times = z * x;
+		
+		System.out.println(" The multiplication of 32 and 52 is" + " " + Times);
+	}
+}

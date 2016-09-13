c24440d525dc075cfc2c49ddb0f0cf154d7f7beb
Julian Lange <langej0159@gmail.com>
2016-09-13 10:38:49 -0700

    lesson 3

---
+++ APLesson_03/Ex file/Ex_01.java
@@ -0,0 +1,13 @@
+Import java.util.Scanner; 
+public class Ex_01
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Hello my name is R00debot");
+		System.out.println("I am here to ask you a few questions and comment it harshly");
+		Scanner keyboard = new Scanner(System.in);
+		
+		//prompts for user input
+		System.out.println(" What is your name? ");
+	}
+}

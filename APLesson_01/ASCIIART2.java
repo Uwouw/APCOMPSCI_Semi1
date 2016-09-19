7e03b582d5f7b984b65c2580b10ab2a3d861dd41
Julian Lange <langej0159@gmail.com>
2016-09-19 11:24:50 -0700

    changes

--- APLesson_01/ASCIIART2.java
+++ APLesson_01/ASCIIART2.java
@@ -3,13 +3,29 @@ public class ASCIIART2
 	public static void main(String[]args)
 	{
 		System.out.println("Ness from the game: Earthbound");
+		System.out.println("     @@@@@@");
+		System.out.println("   @%; @; @@;@");
+		System.out.println("@@@@%@;%%%@");
+		System.out.println("@-   @%%%%@%@");
 		System.out.println(" @%     %%%@%%@");
+		System.out.println(" @@@+@@@@++@@");
+		System.out.println("@@@+++@@+++@@");
+		System.out.println("@+@; ; @; ; ; @; ;@;@");
+		System.out.println("@+; ; ;  @; ; ; @; ; ;;@");
+		System.out.println(" @+; ; ; ; ; ; ; ; ; ; +@");
+		System.out.println("  @; ; ; ; ; ; ; ; ; ; @");
 		System.out.println("    @; ; @@@; ; @");
 		System.out.println("      @+; ; ; +@");
 		System.out.println("   @ @@+@@ @");
+		System.out.println("  @@@       @@@");
 		System.out.println(" @;  @@@@@   @");
+		System.out.println("@ ;@         @ ;@");
+		System.out.println("@;;@@@@@@@;@");
 		System.out.println(" @@               @@@");
 		System.out.println("  @    @ @--%@");
+		System.out.println("   @@  @@-%@");
+		System.out.println("   @%%@@@-%@");
+		System.out.println("  @ %@@ @%@");
 		System.out.println("   @@@   @@@");
 		System.out.println("");
 		System.out.println("");

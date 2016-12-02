6a010030c16835e4cf5d43ae11eea04a405f4456
Julian Lange <langej0159@gmail.com>
2016-12-02 11:06:50 -0800

    Fixed ApLesson 06

--- APLesson_06/Box.java
+++ APLesson_06/Box.java
@@ -4,13 +4,13 @@ public class Box
 	public static void main(String[]args)
 	{
 		Scanner user_input = new Scanner(System.in);
-		System.out.println("Hello I am Mr.Recursion\n");
-		System.out.println("Enter a word");
+		System.out.println("Hello I am Mr.Recursion");
+		System.out.print("Enter a word:: ");
 		String word = user_input.next();
 		
 		for(int i = 0; i <= word.length(); i++)
 		{
-			System.out.print(word);
+			System.out.println(word);
 		
 		}
 	}

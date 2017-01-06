bb435b2816b201d63b146f985fc8f588b3e7727b
Julian Lange <langej0159@gmail.com>
2017-01-06 10:39:47 -0800

    AP Lesson 10

---
+++ APLesson_10/ExpressionSolver.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+public class ExpressionSolver
+{
+	public static void main(String[]args)
+	{
+		Scanner user_input = new Scanner(System.in);
+		integer[] equation = user_input.nextInt();
+		System.out.print(doEquation(equation));
+	}
+	
+	public static void doEquation(ArrayList<Integer> equation)
+	{
+		ArrayList<integer> equation = new ArrayList<Integer>(Array.List(equation));
+		while(i > equation)
+		{
+			if(ArrayList.get(*) || ArrayList.get(/))
+			{
+				if(ArrayList.get(*))
+				{
+					ArrayList.set(i, i--);
+					ArrayList.set(*, i++);
+				}
+				
+				else
+				{
+					ArrayList.set(/, i++);
+					ArrayList.remove(i--);
+					ArrayList.remove(i);
+				}
+				
+			}
+			i++;
+			
+		}
+		while(i > )
+		{
+			if(i < 0 || i > 0)
+			{
+				if(i > 0)
+				{
+					
+				}
+			}
+			i++;
+		}
+		
+		
+		return equation;
+	}
+}

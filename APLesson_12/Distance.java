e865aab6903c5280c40b93efe9cecbe095e5be1a
Julian Lange <langej0159@gmail.com>
2017-01-27 00:30:08 -0800

    AP Lesson 12

--- APLesson_12/Distance.java
+++ APLesson_12/Distance.java
@@ -1,16 +1,29 @@
 import java.util.*;
-public class MPHRunner {
-    static Scanner user_input = new Scanner(System.in);
-    static int d, hrs, min;
-    public static void main(String[]args){
-        System.out.println("Enter a distance");
-        d = user_input.nextInt();
-        System.out.println("Enter the hours");
-        hrs = user_input.nextInt();
-        System.out.println("Enter the minutes");
-        min = user_input.nextInt();
-        MPH miles = new MPH();
-        miles.setVars(d, hrs, min);
-        System.out.println(d + " miles in " + hrs + " hours and " + min + " minutes results in a speed of " + miles.getMPH() + " MPH.");
+public class Distance {
+    private int x1, y1, x2, y2;
+    private double d;
+    public Distance() {
+        x1 = 0;
+        y1 = 0;
+        x2 = 0;
+        y2 = 0;
+        d = 0;
     }
+    public Distance(int m_x, int m_y, int m_z, int m_xy) {
+        x1 = m_x;
+        y1 = m_y;
+        x2 = m_z;
+        y2 = m_xy;
+        d = 0;
+    }
+    public void setValues(int m_x, int m_y, int m_z, int m_xy) {
+        x1 = m_x;
+        y1 = m_y;
+        x2 = m_z;
+        y2 = m_xy;
+    }
+    public double getDistance() {
+        return  Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+    }
+
 }

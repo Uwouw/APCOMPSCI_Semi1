2f640ba7b2a1b868f5bf434e87ac394164d1ff68
Uwouw <langej0159@gmail.com>
2017-02-03 10:47:51 -0800

    AP Lesson 12

--- APLesson_12/MPH.java
+++ APLesson_12/MPH.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class MPH
 {
 	private int distance, hours, minutes;
+	private double mph;
 	public MPH()
 	{
 		distance = 0;
@@ -27,6 +28,16 @@ public class MPH
 	{
 		mph = Math.round(distance / (hours + minutes / 60));
 		return mph;
+	}
+	public static void main(String[]args)
+	{
+		Scanner user_input = new Scanner(System.in);
+		System.out.println("Please input a distasnce, hours, and minutes");
+		int distance = user_input.nextInt();
+		int hours = user_input.nextInt();
+		int minutes = user_input.nextInt();
+		MPH object = new MPHS(int distance, int hours, int minutes);
+		System.out.println(distance + " miles in " + hours + " hours = " + mph + " mph");
 	}
 	
 }
